25549ff53c8fd5c9e782225f4d8307d121ff2537
Mark Jones <jonesaws@amazon.com>
2021-07-09 16:37:15 +1000

    added styling, and commited CFN changes

--- Site/js/app.js
+++ Site/js/app.js
@@ -3,13 +3,21 @@
 // Renders the page based on the current URL
 function renderApp() {
     var content;
+    var title;
     if (window.location.pathname === '/about') {
-      content = '<div>Welcome to the About page</div>'
-    } else if (window.location.pathname === '/') {
-      content = '<div>Welcome Serverless Developer :)</div>'
+      title = "SPA - About";
+      content = '<h1>Welcome to the About page</h1><p>This a simple demonstration of a SPA behind CloudFront, served from S3 with deep linking capability.</p>';
     }
-  
+    else if (window.location.pathname === '/contact') {
+        title = "SPA - Contact";
+        content = '<h1>Welcome to the Contact page</h1><p>This a simple demonstration of a SPA behind CloudFront, served from S3 with deep linking capability.</p>';
+    }
+    else if (window.location.pathname === '/') {
+      title = "SPA - Home";
+      content = '<h1>Welcome to the Demo Single Page App :)</h1><p>This a simple demonstration of a SPA behind CloudFront, served from S3 with deep linking capability.</p>'
+    }
     var main = document.getElementsByTagName('main')[0];
+    document.title = title;
     main.innerHTML = content;
   }
   
